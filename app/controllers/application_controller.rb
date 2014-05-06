8a22fa80660039197b95669b2af8c224f2c080c4
Ismail <isyed@ubuntu.(none)>
2014-05-06 10:17:25 -0400

    fixed devise strong parameter issue. :first_name, :last_name, :profile_name properly saving in database now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   	courses_path
  	end
 
+	before_filter :update_sanitized_params, if: :devise_controller?
+
+	def update_sanitized_params
+	  devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:first_name, :last_name, 
+	  																					:profile_name, :email, :password, 
+	  																					:password_confirmation)}
+	end
+
 end
