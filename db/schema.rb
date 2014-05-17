ca6a45d1b4c3a4de5484aeb36cb5d011aee7a21d
isyed867 <isyed867@gmail.com>
2014-05-17 02:14:29 -0400

    added grade attribute to courses model and displayed it in course index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506131522) do
+ActiveRecord::Schema.define(version: 20140517055326) do
 
   create_table "components", force: true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140506131522) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "grade",      default: 0
   end
 
   create_table "evaluations", force: true do |t|
