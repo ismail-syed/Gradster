8a6ee1eae78a742cb7f61ea2236c4e9a1a0bb7b6
isyed867 <isyed867@gmail.com>
2014-08-07 12:10:29 -0400

    components remove and add working

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,7 +1,7 @@
 class Component < ActiveRecord::Base
 	belongs_to :course
 	has_many :evaluations, :dependent => :destroy
-	accepts_nested_attributes_for :evaluations
+	accepts_nested_attributes_for :evaluations, allow_destroy: true
 	validates :name, presence: true
 	validates :component_weight, presence: true
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
 	belongs_to :users
 	has_many :components, :dependent => :destroy
-	accepts_nested_attributes_for :components
+	accepts_nested_attributes_for :components, allow_destroy: true
 
 	# VALIDATIONS
 	validates :name, presence: true
