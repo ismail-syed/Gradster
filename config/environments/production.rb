2289c749bab08d98cb66be7388527c8d756b1fac
isyed867 <isyed867@gmail.com>
2014-08-19 02:27:21 -0400

    config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Gradster::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier COMMMENT OUT BY ISMAIL
-  config.assets.compress = false
+  config.assets.js_compressor = :uglifier 
+  # config.assets.compress = false
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
