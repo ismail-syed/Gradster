7970aca0624f0266aef96ee5e10e6340c7b9977e
isyed867 <isyed867@gmail.com>
2014-08-19 00:47:07 -0400

    rejects blank component fields

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,11 @@
 class Course < ActiveRecord::Base
 	belongs_to :users
 	has_many :components, :dependent => :destroy
-	accepts_nested_attributes_for :components, allow_destroy: true
+	accepts_nested_attributes_for :components, allow_destroy: true, 
+								  reject_if: proc { |attributes| 
+								  	attributes['name'].blank? 
+								  	attributes['component_name'].blank? 
+								  }
 
 	# VALIDATIONS
 	validates :name, presence: true
