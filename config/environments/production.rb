63fc95db4f279b51148b876992b8a4c60f3dc3e9
isyed867 <isyed867@gmail.com>
2014-08-19 02:18:02 -0400

    production config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Gradster::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  config.assets.compress = false
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
