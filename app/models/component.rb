721bdb2615730d225c3177266eb1fb120bd39e98
isyed867 <isyed867@gmail.com>
2014-08-19 01:28:11 -0400

    ready to merge, hopefully

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,7 +1,7 @@
 class Component < ActiveRecord::Base
 	belongs_to :course
 	has_many :evaluations, :dependent => :destroy
-	accepts_nested_attributes_for :evaluations, :allow_destroy => true, reject_if: proc { |attributes| attributes['mark'].blank? }
+	accepts_nested_attributes_for :evaluations, :allow_destroy => true
 	validates :name, presence: true
 	validates :component_weight, presence: true
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,11 +1,7 @@
 class Course < ActiveRecord::Base
 	belongs_to :users
 	has_many :components, :dependent => :destroy
-	accepts_nested_attributes_for :components, allow_destroy: true, 
-								  reject_if: proc { |attributes| 
-								  	attributes['name'].blank? 
-								  	attributes['component_name'].blank? 
-								  }
+	accepts_nested_attributes_for :components, allow_destroy: true
 
 	# VALIDATIONS
 	validates :name, presence: true
