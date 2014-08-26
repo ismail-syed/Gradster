77468fd922a4a57ad4c10da125d3131f774d1efa
isyed867 <isyed867@gmail.com>
2014-08-26 00:23:12 -0400

    updated form_for erros styling

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,6 +1,6 @@
 class Evaluation < ActiveRecord::Base
 	belongs_to :component
-	validates :mark, presence: true
+	validates :mark, presence: true, :inclusion => 0..125
 
 	def average_evaluation_mark
 		self.class.average(:mark).where(:component_id => self.component_id)
