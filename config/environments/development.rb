b1f0c34afdf728f6c33c2e23e9e876ec6cbf87ef
isyed867 <isyed867@gmail.com>
2014-08-26 02:23:54 -0400

    config change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Gradster::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'http://gradster.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
