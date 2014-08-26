8fd1672daecafb0f5efaaf88d43883ab7dda1d11
isyed867 <isyed867@gmail.com>
2014-08-26 02:14:04 -0400

    changed mailer url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Gradster::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://gradster.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
