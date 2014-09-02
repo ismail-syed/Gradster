3767c2c050f35b9b6a469ce30d7db2939bcf2ee4
Ismail Syed <isyed867@gmail.com>
2014-09-02 19:30:57 -0400

    flud database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828164252) do
+ActiveRecord::Schema.define(version: 20140902232723) do
 
   create_table "components", force: true do |t|
     t.string   "name"
