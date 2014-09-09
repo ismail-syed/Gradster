f5fc015ef8d2a7923bdd0673e5300e0db9386074
Ismail Syed <isyed867@gmail.com>
2014-09-09 15:30:18 -0400

    changed login to sign in and mailer address to noreply@gradster.ca

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'GradsterAdmin@gradster.com'
+  config.mailer_sender = 'noreply@gradster.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
